171b1d7edffb2e30e9eaeb94c57f051c174974cc
Shen Junru <shenjunru@gmail.com>
2022-06-08 11:38:37 +0800

    bump: version 0.6.0

--- lib/types/index.d.ts
+++ lib/types/index.d.ts
@@ -6,10 +6,14 @@ declare type KeepAliveProps = {
     children: React.ReactNode;
 };
 export declare const KeepAlive: React.FC<KeepAliveProps> & {
-    Provider: React.Provider<HTMLElement | null>;
+    Provider: React.FC<{
+        children: React.ReactNode;
+        value: null | HTMLElement;
+    }>;
 };
 export declare function keepAlive<P>(Component: React.ComponentType<P>, getProps: (props: P) => ((Omit<KeepAliveProps, 'children'> & {
     key?: React.Key;
 }) | string)): React.FC<P>;
+export declare const useIgnoreKeepAlive: () => (name: string) => void;
 export { markClassComponentHasSideEffectRender, markEffectHookIsOnetime, };
 export default KeepAlive;
