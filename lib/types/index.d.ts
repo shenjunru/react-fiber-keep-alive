ed4adc83ce2771d6364ba3dc474e062d713b9c90
Shen Junru <shenjunru@gmail.com>
2022-06-11 13:32:23 +0800

    bump: version 0.6.1

--- lib/types/index.d.ts
+++ lib/types/index.d.ts
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { markClassComponentHasSideEffectRender, markEffectHookIsOnetime } from './helpers';
 declare type KeepAliveProps = {
     name: string;
