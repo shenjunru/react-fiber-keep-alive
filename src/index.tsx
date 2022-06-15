6e959036038c4e8c76664db6b439733436891b3c
Shen Junru <shenjunru@gmail.com>
2022-06-15 12:47:09 +0800

    feat: move internal storages to context

--- src/index.tsx
+++ src/index.tsx
@@ -27,10 +27,6 @@ const enum Step {
     Effect = 0b0010,
 }
 
-const randomKey = Math.random().toString(36).slice(2);
-const CachesPropKey: unique symbol = ('__keepAliveCaches$' + randomKey) as any;
-const MapperPropKey: unique symbol = ('__keepAliveMapper$' + randomKey) as any;
-
 type KeepAliveCache = [Fiber, { current: boolean }];
 type KeepAliveState = [Step, null | KeepAliveCache];
 
@@ -40,28 +36,23 @@ type KeepAliveProps = {
     children: React.ReactNode;
 };
 
-type RootContainer = null | (HTMLElement & {
-    [CachesPropKey]?: Map<string, KeepAliveCache>,
-    [MapperPropKey]?: Map<string, string>,
-});
+type Context = Readonly<[] | [HTMLElement, Map<string, KeepAliveCache>,  Map<string, string>]>;
 
-const KeepAliveContext = React.createContext<null | HTMLElement>(null);
+const KeepAliveContext = React.createContext<Context>([]);
 
 const KeepAliveProvider: React.FC<{
     children: React.ReactNode;
     value: null | HTMLElement;
 }> = (props) => {
-    const context: RootContainer = props.value as any;
-
-    React.useEffect(() => () => {
-        if (context) {
-            delete context[CachesPropKey];
-            delete context[MapperPropKey];
-        }
-    }, [context]);
+    const container = props.value;
+    const context: Context = React.useMemo(() => {
+        return container ? [container, new Map(), new Map()] : [];
+    }, []);
 
     return (
-        <KeepAliveContext.Provider {...props} />
+        <KeepAliveContext.Provider value={context}>
+            {props.children}
+        </KeepAliveContext.Provider>
     );
 };
 
@@ -76,19 +67,19 @@ const KeepAliveEffect: React.FC<{
     state: KeepAliveState;
     name: string;
 }> = (props) => {
-    const context = React.useContext(KeepAliveContext);
+    const [container] = React.useContext(KeepAliveContext);
     const cursor = props.cursor;
     const state = props.state;
     const [step] = state;
 
     useIsomorphicLayoutEffect(() => {
-        if (!context || step !== Step.Effect) {
+        if (!container || step !== Step.Effect) {
             return;
         }
 
         // console.log('[KEEP-ALIVE] [LIVE]', props.name);
 
-        const rootFiber = getRootFiber(context);
+        const rootFiber = getRootFiber(container);
         const cursorFiber = findFiber(rootFiber, (fiber) => fiber.stateNode === cursor.current);
         const effectFiber = cursorFiber?.sibling;
         const renderFiber = effectFiber?.sibling;
@@ -123,31 +114,17 @@ const KeepAliveFinish: React.FC<{
 };
 
 const KeepAliveManage: React.FC<KeepAliveProps> = (props) => {
-    const context: RootContainer = React.useContext(KeepAliveContext) as any;
+    const [container, caches, mapper] = React.useContext(KeepAliveContext);
     const cursor = React.useRef<KeepAliveCursor>(null);
-    const caches = React.useMemo((): Map<string, KeepAliveCache> => {
-        const value = context?.[CachesPropKey] || new Map();
-        if (context) {
-            context[CachesPropKey] = value;
-        }
-        return value;
-    }, []);
-    const mapper = React.useMemo((): Map<string, string> => {
-        const value = context?.[MapperPropKey] || new Map();
-        if (context) {
-            context[MapperPropKey] = value;
-        }
-        return value;
-    }, []);
 
-    const readKey = React.useMemo(() => mapper.get(props.name) || props.name, []);
-    props.name && mapper.set(props.name, readKey);
+    const readKey = React.useMemo(() => mapper?.get(props.name) || props.name, []);
+    props.name && mapper?.set(props.name, readKey);
 
     const [state, setState] = React.useState<KeepAliveState>(() => {
-        const _cache = caches.get(readKey);
-        caches.delete(readKey);
+        const _cache = caches?.get(readKey);
+        caches?.delete(readKey);
 
-        props.ignore && mapper.forEach((value, key) => {
+        props.ignore && mapper?.forEach((value, key) => {
             if (value === readKey) {
                 mapper.delete(key);
             }
@@ -162,11 +139,11 @@ const KeepAliveManage: React.FC<KeepAliveProps> = (props) => {
     ignore.current = true === props.ignore;
 
     useIsomorphicLayoutEffect(() => () => {
-        if (!context || !readKey || ignore.current || bypass.current) {
+        if (!container || !caches || !readKey || ignore.current || bypass.current) {
             return;
         }
 
-        const rootFiber = getRootFiber(context);
+        const rootFiber = getRootFiber(container);
         const cursorFiber = findFiber(rootFiber, (fiber) => fiber.stateNode === cursor.current);
         const renderFiber = cursorFiber?.sibling?.sibling;
         if (!renderFiber) {
@@ -188,12 +165,12 @@ const KeepAliveManage: React.FC<KeepAliveProps> = (props) => {
             return;
         }
 
-        if (!context || !readKey || !cache) {
+        if (!container || !readKey || !cache) {
             // console.log('[KEEP-ALIVE]', '[SKIP]', name);
             return setState([Step.Finish, cache]);
         }
 
-        const rootFiber = getRootFiber(context);
+        const rootFiber = getRootFiber(container);
         const cursorFiber = findFiber(rootFiber, (fiber) => fiber.stateNode === cursor.current);
         const renderFiber = cursorFiber?.sibling?.sibling;
         if (!renderFiber || !renderFiber) {
@@ -257,11 +234,9 @@ export function keepAlive<P>(
 }
 
 export const useIgnoreKeepAlive = () => {
-    const context: RootContainer = React.useContext(KeepAliveContext) as any;
+    const [, caches, mapper] = React.useContext(KeepAliveContext);
 
     return React.useCallback((name: string) => {
-        const caches = context?.[CachesPropKey];
-        const mapper = context?.[MapperPropKey];
         const readKey = mapper?.get(name);
         readKey && caches?.delete(readKey);
         readKey && mapper?.forEach((value, key) => {
@@ -269,7 +244,7 @@ export const useIgnoreKeepAlive = () => {
                 mapper.delete(key);
             }
         });
-    }, [context]);
+    }, [caches, mapper]);
 };
 
 export {
@@ -278,4 +253,3 @@ export {
 };
 
 export default KeepAlive;
-
