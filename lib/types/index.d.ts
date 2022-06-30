ba06125060ac81ae09cca97ed88a492f657db026
Shen Junru <shenjunru@gmail.com>
2022-06-30 21:17:06 +0800

    bump: version 0.7.0

--- lib/types/index.d.ts
+++ lib/types/index.d.ts
@@ -1,11 +1,26 @@
+import type { Fiber } from 'react-reconciler';
 import * as React from 'react';
 import { markClassComponentHasSideEffectRender, markEffectHookIsOnetime } from './helpers';
-declare type KeepAliveProps = {
+declare const enum Step {
+    Finish = 0,
+    Render = 1,
+    Effect = 2
+}
+export declare type KeepAliveCache = [Fiber, {
+    current: boolean;
+}];
+export declare type KeepAliveState = [Step, null | KeepAliveCache];
+export declare type KeepAliveProps = {
     name: string;
     ignore?: boolean;
+    onSave?: (name: string) => void;
+    onRead?: (name: string) => void;
     children: React.ReactNode;
 };
+declare type Context = Readonly<[] | [HTMLElement, Map<string, KeepAliveCache>, Map<string, string>]>;
+export declare const KeepAliveContext: React.Context<Context>;
 export declare const KeepAlive: React.FC<KeepAliveProps> & {
+    Context: React.Context<Context>;
     Provider: React.FC<{
         children: React.ReactNode;
         value: null | HTMLElement;
